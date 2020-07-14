9ec5fb65fc4866de0e690225e4cfb1f402ee0d3e
Alexander <bykovskiyan@gmail.com>
2020-07-14 13:50:04 +0300

    Created App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Layout from './hoc/Layout/Layout';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout>
+      <h1 style={{ 'border': '1px solid red', 'padding': '20px', 'margin': 0 }}>
+        Test
+      </h1>
+    </Layout>
   );
 }
 
