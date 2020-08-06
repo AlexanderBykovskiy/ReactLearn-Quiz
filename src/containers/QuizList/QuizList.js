2863f9bf0f513441f0b224885e89a9b9a02ed9d4
AlexanderBykovskiy <bykovskiyan@gmail.com>
2020-08-06 17:56:57 +0300

    Added Quize list page

--- src/containers/QuizList/QuizList.js
+++ src/containers/QuizList/QuizList.js
@@ -1,10 +1,31 @@
 import React from 'react';
 import classes from './QuizList.module.css';
+import { NavLink } from 'react-router-dom';
 
 class QuizList extends React.Component {
+
+    renderQuizes() {
+        return [1, 2, 3].map((item, index) => {
+            return (
+                <li key={index}>
+                    <NavLink
+                        to={'/quiz/'+ item}
+                    >
+                        Test {item}
+                    </NavLink>
+                </li>
+            );
+        });
+    }
+
     render() {
         return (
-            <div className={classes.quizlist}>QuizList</div>
+            <div className={classes.quizlist}>
+                <h1>QuizList</h1>
+                <ul>
+                    {this.renderQuizes()}
+                </ul>
+            </div>
         )
     }
 }
