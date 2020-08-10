9373abcf3ef9fafdfb24de3f82ed8883c3ce4521
AlexanderBykovskiy <bykovskiyan@gmail.com>
2020-08-10 18:50:50 +0300

    Maked form Auth

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,10 +1,30 @@
 import React from 'react';
+import Button from '../../components/UI/Button/Button'
 import classes from './Auth.module.css';
 
 class Auth extends React.Component {
     render() {
         return (
-            <div className={classes.auth}>Auth</div>
+            <div className={classes.auth}>
+                <div className={classes.quizWrapper}>
+                    <h1>Auth</h1>
+                    <div className={classes.content}>
+                        <form action="">
+                            <input type="text"/>
+                            <input type="text"/>
+                            <Button
+                                caption="Sign IN"
+                                onClickfunction={this.handleSignIN}
+                            />
+                            <Button
+                                type="primary"
+                                caption="Sign UP"
+                                onClickfunction={this.handleSignUP}
+                            />
+                        </form>
+                    </div>
+                </div>
+            </div>
         )
     }
 }
